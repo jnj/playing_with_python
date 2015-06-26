cd6d432962f3b2e6d7c4fd5700ad402842f6b0d0
Josh Joyce <j_joyce2001@yahoo.com>
2015-06-25 21:06:29 -0500

    initial commit

---
+++ maya.py
@@ -0,0 +1,27 @@
+import turtle
+
+t = turtle.Pen()
+scr = t.screen
+scr.bgcolor(0.5, 0.5, 0.5)
+t.reset()
+t.color('yellow')
+t.circle(56)
+t.color('green')
+t.circle(57)
+t.color('purple')
+t.circle(58)
+t.color('orange')
+t.circle(59)
+t.color('pink')
+t.circle(60)
+t.color('blue')
+t.circle(61)
+t.color('red')
+t.circle(62)
+t.color('brown')
+t.circle(63)
+t.color('black')
+t.circle(64)
+t.color('yellow')
+t.circle(65)
+
